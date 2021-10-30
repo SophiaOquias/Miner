28fd686d82a6aa2c54dff3669c2da72587b3f8c1
Sophia <sophia.oquias@gmail.com>
2021-10-30 21:55:36 +0800

    created Beacon.java

---
+++ src/com/mine/Beacon.java
@@ -0,0 +1,15 @@
+package com.mine;
+
+public class Beacon extends Piece {
+
+    // Properties
+
+    private int squaresToGold;
+
+
+    // Methods
+
+    public void calculateSquaresToGold(PotOfGold gold) {
+    }
+
+}
