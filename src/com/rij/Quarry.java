386b13a5f731f7f605897bb55e050b0bcaa56b19
rijopalla <rij_opalla@dlsu.edu.ph>
2021-10-31 19:05:48 +0800

    edited constructor to set the size of the mine property (quarry)

--- src/com/rij/Quarry.java
+++ src/com/rij/Quarry.java
@@ -9,12 +9,12 @@ public class Quarry {
 	//Constructor
 	public Quarry(int size) {
 		this.size = size;
+		this.mine = new Piece[size][size]; //mine property gets input sizes
 	}
 	
 	//Methods
 	public void setSize(int input) {
 		this.size = input;
 	}
-	
 
 }
