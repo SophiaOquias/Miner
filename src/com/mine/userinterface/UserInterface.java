66574d884e339aacba03291acc1d01708ba45f97
Jon Llamado <jon_llamado@dlsu.edu.ph>
2021-11-05 23:40:11 +0800

    Finished UI. Untested. No Miner yet

--- src/com/mine/userinterface/UserInterface.java
+++ src/com/mine/userinterface/UserInterface.java
@@ -1,18 +1,28 @@
-package com.mine.userinterface;
+package com.mine.userinterface; 	
 
 import com.mine.board.Quarry;
 import com.mine.pieces.*;
 
 public class UserInterface {
-	public void displayQuarry(Quarry quarry) {
+	public void displayQuarry(Quarry quarry, Miner miner) {
+		int size = quarry.getSize();
 		
+		for(int i = 0; i < size; i++)
+			for(int j = 0; j < size; j++) {
+				if(miner.getCurrentPosition().getX() == i && miner.getCurrentPosition().getY() == j) displayMiner();
+				else displayPiece(quarry.getMine()[i][j]);
+			}
 	}
 	
 	private void displayPiece(Piece piece) {
-		
+		if (piece instanceof Pit) System.out.print("P");
+		else if (piece instanceof PotOfGold) System.out.print("G");
+		else if (piece instanceof Beacon) System.out.print("B");
+		else if (piece instanceof Stone) System.out.print("-");
+		else System.out.print("X");
 	}
 	
-	private void displayMiner(/*Miner miner*/) {
-		
+	private void displayMiner() {
+		System.out.print("M");
 	}
 }
