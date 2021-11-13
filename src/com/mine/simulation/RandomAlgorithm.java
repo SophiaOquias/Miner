ee655e438ba7e284f9b9428956f520bdcba7eed1
Sophia <sophia.oquias@gmail.com>
2021-11-13 17:46:24 +0800

    removed main

--- src/com/mine/simulation/RandomAlgorithm.java
+++ src/com/mine/simulation/RandomAlgorithm.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class RandomAlgorithm {
 
-    public static void main(String[] args) {
+    public void randomAlgorithm() {
 
         // get size of quarry
         Scanner sc = new Scanner(System.in);
