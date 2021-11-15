b9b3a28fc85e4853b03130ce1844ffdcf1ac5c27
rijopalla <rij_opalla@dlsu.edu.ph>
2021-11-16 01:13:44 +0800

    added temp class for SmartGUI

---
+++ src/com/mine/userinterface/SmartGUI.java
@@ -0,0 +1,40 @@
+package com.mine.userinterface;
+
+import com.mine.pieces.*;
+import com.mine.board.*;
+import com.mine.miner.Miner;
+import com.mine.miner.MiningManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class SmartGUI extends JPanel {
+	
+	//Properties
+	private Quarry quarry;
+	private Miner miner;
+	private MiningManager manager;
+	private Image[] images;
+	
+	//Constants
+	private final int IMG_SIZE = 30;
+	private final int OFFSET = 50;
+	
+	//Image constants
+	private final int NUM_IMG = 5;
+	private final int MINER = 1;
+	private final int BEACON = 2;
+	private final int PIT = 3;
+	private final int GOLD = 4;
+	private final int STONE = 5;
+	
+	public SmartGUI() {
+		//temp
+	}
+	
+	public void smartAlgorithm() {
+		//temp
+	}
+
+}
