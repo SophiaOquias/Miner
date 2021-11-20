eb89d4d5d2a4b2d67bd5a49455bf5d8ce3ecccc0
rijopalla <rij_opalla@dlsu.edu.ph>
2021-11-20 16:56:54 +0800

    added smart algorithm

--- src/com/mine/userinterface/SmartGUI.java
+++ src/com/mine/userinterface/SmartGUI.java
@@ -29,12 +29,78 @@ public class SmartGUI extends JPanel {
 	private final int GOLD = 4;
 	private final int STONE = 5;
 	
-	public SmartGUI() {
-		//temp
+	public SmartGUI(int size) {
+		this.quarry = new Quarry(size);
+		this.manager = new MiningManager(quarry, miner);
 	}
 	
+	public boolean minerCompare(Miner m1, Miner m2) {
+	    if(m1.getX() == m2.getX() &&
+	        m1.getY() == m2.getY() &&
+	        m1.getFront() == m2.getFront())
+	        return true;
+	   return false;
+	}
+	
+	@SuppressWarnings("unused")
+	public boolean isInList(ArrayList<Miner> nodeList, Miner node) {
+		for(int i = 0; i < nodeList.size(); i++) {
+		    if(minerCompare(nodeList.get(i), node));
+		        return true; 
+		}
+		
+		return false; 
+    }
+	
 	public void smartAlgorithm() {
-		//temp
+		Stack<Miner> nodeList = new Stack<>();
+		ArrayList<Miner> exploredNodes = new ArrayList<>();
+		
+
+		exploredNodes.add(new Miner());
+
+		while(!nodeList.isEmpty() && !manager.isOnPotOfGold(quarry, miner)) {
+			exploredNodes.add(nodeList.pop()); 
+			
+			for(int i = 0; i < 2; i++) {
+				Miner[] tempList = new Miner[2];
+				
+				// adding possible actions to tempList 
+				if(i == 0) { // move action 
+				    tempList[i] = new Miner();
+				    if(!(manager.isMinerFacingEdge(quarry, miner))) {
+				        tempList[i].move();
+                        tempList[i].setHeuristicValue(tempList[i].scan(quarry));
+				    }
+				    else {
+                        tempList[i] = null; 
+				    }
+				}
+				else { //rotate action
+				    tempList[i] = new Miner();
+				    tempList[i].rotate();
+				    tempList[i].setHeuristicValue(tempList[i].scan(quarry));
+				}
+				
+				// adding nodes to stack based on greater h(n) value 
+				if(isInList(exploredNodes, tempList[0]) || tempList[0] == null) {
+				    nodeList.push(tempList[1]); 							
+				}
+				else if(isInList(exploredNodes, tempList[1])) {
+				    nodeList.push(tempList[0]);
+				}
+				//check if moveNode's h(n) >= rotateNode's h(n)
+				else if(tempList[0].getHeuristicValue() >= tempList[1].getHeuristicValue()) {
+					nodeList.push(tempList[1]); // push rotate node
+					nodeList.push(tempList[0]); // push move node
+				}
+				else {
+					nodeList.push(tempList[0]); // push move node
+					nodeList.push(tempList[1]); // push rotate node
+				}
+				
+			}
+		}
 	}
 
 }
