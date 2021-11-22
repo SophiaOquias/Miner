b2f9bdd3b771c3bb650d2e09b0be2b7e2e50ca90
Sophia <sophia.oquias@gmail.com>
2021-11-22 15:59:26 +0800

    added stuff

--- src/com/mine/simulation/SmartAlgorithm.java
+++ src/com/mine/simulation/SmartAlgorithm.java
@@ -1,10 +1,28 @@
 package com.mine.simulation;
 
-import com.mine.pieces.*;
-import com.mine.board.*;
+import javax.swing.*;
+import com.mine.userinterface.SmartGUI;
 
-import java.util.*;
+import java.awt.*;
 
-public class SmartAlgorithm {
+public class SmartAlgorithm extends JFrame {
 
+    public SmartAlgorithm() {
+        int size = 8;
+        this.add(new SmartGUI(size));
+
+        this.pack();
+
+        this.setTitle("Smart Rational Level Miner");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null); // makes window open on center
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            SmartAlgorithm smart = new SmartAlgorithm();
+            smart.setVisible(true);
+        });
+    }
 }
