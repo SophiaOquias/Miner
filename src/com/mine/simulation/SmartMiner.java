0324367fc642c5122427d3c37ecafd60f3538411
Sophia <sophia.oquias@gmail.com>
2021-11-22 20:16:52 +0800

    rearranged files and packages

---
+++ src/com/mine/simulation/SmartMiner.java
@@ -0,0 +1,218 @@
+package com.mine.simulation;
+
+import com.mine.pieces.*;
+import com.mine.board.*;
+import com.mine.miner.Miner;
+import com.mine.miner.MiningManager;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+public class SmartMiner extends JPanel {
+	
+	//Properties
+	private Quarry quarry;
+	private Miner miner;
+	private MiningManager manager;
+	private Stack<Miner> nodeList;
+	private ArrayList<Miner> exploredNodes;
+
+	//GUI Components
+	private Image[] images;
+	private Timer timer;
+	private JButton playButton;
+	private JButton stepButton;
+	
+	//Constants
+	private final int IMG_SIZE = 30;
+	private final int OFFSET = 50;
+	private final int DELAY = 140;
+	
+	//Image constants
+	private final int NUM_IMG = 8;
+	private final int STONE = 0;
+	// 1 - 4 is reserved for MINER
+	private final int BEACON = 5;
+	private final int PIT = 6;
+	private final int GOLD = 7;
+	
+	public SmartMiner(int size) {
+		this.initBoard(size);
+		this.loadImages();
+
+		// sets size of panel
+		this.setPreferredSize(
+				new Dimension(
+						size * IMG_SIZE,
+						size * IMG_SIZE + OFFSET
+				)
+		);
+
+		// initializes timer
+		this.timer = new Timer(DELAY, new FastPlay());
+
+		// initializes buttons
+		this.playButton = new JButton("Play");
+		this.stepButton = new JButton("Play One Step");
+
+		// adds ActionListeners to buttons
+		this.playButton.addActionListener(new FastPlay());
+		this.stepButton.addActionListener(new OneStep());
+
+		// adds buttons to panel
+		this.add(this.playButton);
+		this.add(this.stepButton);
+	}
+
+	private void initBoard(int size) {
+		this.quarry = new Quarry(size);
+		this.miner = new Miner();
+		this.manager = new MiningManager(quarry, miner);
+
+		this.nodeList = new Stack<>();
+		this.nodeList.push(this.miner);
+		this.exploredNodes = new ArrayList<>();
+	}
+
+	private void loadImages() {
+		images = new Image[NUM_IMG];
+
+		for(int i = 0; i < NUM_IMG; i++) {
+			var path = "src/images/" + i + ".png";
+			this.images[i] = (new ImageIcon(path)).getImage().getScaledInstance(IMG_SIZE, IMG_SIZE, 1);
+		}
+	}
+	
+	private boolean minerCompare(Miner m1, Miner m2) {
+	    if(m1.getX() == m2.getX() &&
+	        m1.getY() == m2.getY() &&
+	        m1.getFront() == m2.getFront())
+	        return true;
+	   return false;
+	}
+
+	private boolean isInList(ArrayList<Miner> nodeList, Miner node) {
+		for(int i = 0; i < nodeList.size(); i++) {
+		    if(minerCompare(nodeList.get(i), node))
+		        return true; 
+		}
+		
+		return false; 
+    }
+	
+	public void bestFirstSearch() {
+
+		// will have to change while to if statement if using javax.swing.Timer
+		if(!this.nodeList.isEmpty() && !this.manager.isOnPotOfGold(this.quarry, this.miner)) {
+			this.timer.start();
+			this.miner = nodeList.peek();
+			repaint();
+			this.exploredNodes.add(nodeList.pop());
+			this.expand();
+		}
+		else {
+			this.timer.stop();
+		}
+	}
+
+	private void expand() {
+		// instantiating new miner nodes
+		Miner moveMiner = new Miner();
+		Miner rotateMiner = new Miner();
+
+		// copy information of current state of miner
+		copy(moveMiner, miner);
+		copy(rotateMiner, miner);
+
+		// performs move only if miner is not facing edge 
+		if(!manager.isMinerFacingEdge(quarry, miner)) {
+			moveMiner.move();
+			moveMiner.setHeuristicValue(moveMiner.scan(quarry));
+		}
+
+		rotateMiner.rotate();
+		rotateMiner.setHeuristicValue(rotateMiner.scan(quarry));
+
+		// pushes only rotateMiner if miner is facing edge (OOB) or node has already been explored
+		if(manager.isMinerFacingEdge(quarry, miner) ||
+				isInList(exploredNodes, moveMiner) && !isInList(exploredNodes, rotateMiner)) {
+			nodeList.push(rotateMiner);
+		}
+		// pushes only moveMiner if node has already been explored
+		else if(isInList(exploredNodes, rotateMiner) && !isInList(exploredNodes, rotateMiner)) {
+			nodeList.push(moveMiner);
+		}
+		// check if moveNode's h(n) >= rotateNode's h(n)
+		// whichever is pushed last becomes top of stack
+		else if(moveMiner.getHeuristicValue() >= rotateMiner.getHeuristicValue()) {
+			nodeList.push(rotateMiner); // push rotate node
+			nodeList.push(moveMiner); // push move node
+		}
+		else {
+			nodeList.push(moveMiner); // push move node
+			nodeList.push(rotateMiner); // push rotate node
+		}
+	}
+
+	// copies position and orientation of m2 to m1
+	private void copy(Miner m1, Miner m2) {
+		m1.setX(m2.getX());
+		m1.setY(m2.getY());
+		m1.setFront(m2.getFront());
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		for(int i = 0; i < quarry.getSize(); i++) {
+			for(int j = 0; j < quarry.getSize(); j++) {
+
+				int x = j * IMG_SIZE;
+				int y = i * IMG_SIZE + OFFSET;
+				Piece currentPiece = this.quarry.getPiece(i, j);
+
+				// draw all other pieces
+				if(currentPiece instanceof Stone)
+					g.drawImage(this.images[STONE], x, y, this);
+				else if(currentPiece instanceof Pit)
+					g.drawImage(this.images[PIT], x, y, this);
+				else if(currentPiece instanceof Beacon) {
+					g.drawImage(this.images[BEACON], x, y, this);
+					int num = ((Beacon) currentPiece).getSquaresToGold();
+					g.drawString("" + num, x + 10, y + 25);
+				}
+				else if(currentPiece instanceof PotOfGold)
+					g.drawImage(this.images[GOLD], x, y, this);
+			}
+		}
+
+		// draw miner
+		g.drawImage(this.images[miner.getFront()],
+				miner.getY() * IMG_SIZE,
+				miner.getX() * IMG_SIZE + OFFSET,
+				this);
+	}
+
+	private class FastPlay implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			bestFirstSearch();
+		}
+	}
+
+	private class OneStep implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(!nodeList.isEmpty() && !manager.isOnPotOfGold(quarry, miner)) {
+				miner = nodeList.peek();
+				exploredNodes.add(nodeList.pop());
+				expand();
+			}
+		}
+	}
+
+}
--- src/com/mine/userinterface/SmartGUI.java
+++ src/com/mine/userinterface/SmartGUI.java
@@ -1,218 +1,101 @@
 package com.mine.userinterface;
 
-import com.mine.pieces.*;
-import com.mine.board.*;
-import com.mine.miner.Miner;
-import com.mine.miner.MiningManager;
+import com.mine.simulation.SmartMiner;
 
 import javax.swing.*;
-import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
 
-public class SmartGUI extends JPanel {
-	
-	//Properties
-	private Quarry quarry;
-	private Miner miner;
-	private MiningManager manager;
-	private Stack<Miner> nodeList;
-	private ArrayList<Miner> exploredNodes;
+public class SmartGUI {
 
-	//GUI Components
-	private Image[] images;
-	private Timer timer;
-	private JButton playButton;
-	private JButton stepButton;
-	
-	//Constants
-	private final int IMG_SIZE = 30;
-	private final int OFFSET = 50;
-	private final int DELAY = 140;
-	
-	//Image constants
-	private final int NUM_IMG = 8;
-	private final int STONE = 0;
-	// 1 - 4 is reserved for MINER
-	private final int BEACON = 5;
-	private final int PIT = 6;
-	private final int GOLD = 7;
-	
-	public SmartGUI(int size) {
-		this.initBoard(size);
-		this.loadImages();
+    private JFrame smartFrame;
+    private JFrame sliderFrame;
+    private JSlider slider;
+    private JButton okayButton;
+    private JLabel sizeLabel;
+    private int size;
 
-		// sets size of panel
-		this.setPreferredSize(
-				new Dimension(
-						size * IMG_SIZE,
-						size * IMG_SIZE + OFFSET
-				)
-		);
+    public void createSliderFrame() {
 
-		// initializes timer
-		this.timer = new Timer(DELAY, new FastPlay());
+        // instantiating Swing components
+        this.sliderFrame = new JFrame();
+        this.okayButton = new JButton("Okay");
+        this.slider = new JSlider(8, 64);
+        this.sizeLabel = new JLabel("Selected Size: ");
 
-		// initializes buttons
-		this.playButton = new JButton("Play");
-		this.stepButton = new JButton("Play One Step");
+        // sliderFrame settings
+        this.sliderFrame.setSize(450, 200);
+        this.sliderFrame.add(this.slider);
+        this.sliderFrame.add(this.okayButton);
+        this.sliderFrame.add(this.sizeLabel);
+        this.sliderFrame.setLayout(null);
+        this.sliderFrame.setLocationRelativeTo(null); // makes frame start at center
+        this.sliderFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		// adds ActionListeners to buttons
-		this.playButton.addActionListener(new FastPlay());
-		this.stepButton.addActionListener(new OneStep());
+        // "selected size: " text settings
+        this.sizeLabel.setBounds(20, 70, 400, 100);
 
-		// adds buttons to panel
-		this.add(this.playButton);
-		this.add(this.stepButton);
-	}
+        // button settings
+        this.okayButton.setBounds(250, 100, 100, 30);
+        this.okayButton.addActionListener(new ButtonListener());
 
-	private void initBoard(int size) {
-		this.quarry = new Quarry(size);
-		this.miner = new Miner();
-		this.manager = new MiningManager(quarry, miner);
+        // slider settings
+        this.slider.setBounds(10, 0, 400, 100);
+        this.slider.setMinorTickSpacing(1);
+        this.slider.setMajorTickSpacing(8);
+        this.slider.setPaintTicks(true);
+        this.slider.setPaintLabels(true);
+        this.slider.addChangeListener(new SliderListener());
+        this.slider.setVisible(true);
 
-		this.nodeList = new Stack<>();
-		this.nodeList.push(this.miner);
-		this.exploredNodes = new ArrayList<>();
-	}
+        this.sliderFrame.setVisible(true);
 
-	private void loadImages() {
-		images = new Image[NUM_IMG];
-
-		for(int i = 0; i < NUM_IMG; i++) {
-			var path = "src/images/" + i + ".png";
-			this.images[i] = (new ImageIcon(path)).getImage().getScaledInstance(IMG_SIZE, IMG_SIZE, 1);
-		}
-	}
-	
-	private boolean minerCompare(Miner m1, Miner m2) {
-	    if(m1.getX() == m2.getX() &&
-	        m1.getY() == m2.getY() &&
-	        m1.getFront() == m2.getFront())
-	        return true;
-	   return false;
-	}
-
-	private boolean isInList(ArrayList<Miner> nodeList, Miner node) {
-		for(int i = 0; i < nodeList.size(); i++) {
-		    if(minerCompare(nodeList.get(i), node))
-		        return true; 
-		}
-		
-		return false; 
     }
-	
-	public void bestFirstSearch() {
 
-		// will have to change while to if statement if using javax.swing.Timer
-		if(!this.nodeList.isEmpty() && !this.manager.isOnPotOfGold(this.quarry, this.miner)) {
-			this.timer.start();
-			this.miner = nodeList.peek();
-			repaint();
-			this.exploredNodes.add(nodeList.pop());
-			this.expand();
-		}
-		else {
-			this.timer.stop();
-		}
-	}
+    public void createSmartFrame() {
+        this.smartFrame = new JFrame();
+        JScrollPane scrollPane = new JScrollPane(new SmartMiner(this.size));
 
-	private void expand() {
-		// instantiating new miner nodes
-		Miner moveMiner = new Miner();
-		Miner rotateMiner = new Miner();
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(16);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
 
-		// copy information of current state of miner
-		copy(moveMiner, miner);
-		copy(rotateMiner, miner);
+        if (this.size <= 20) {
+            this.smartFrame.setSize(new Dimension(
+                    (size + 2) * 30,
+                    (size + 2) * 30 + 50
+            ));
+        } else {
+            this.smartFrame.setSize(600, 650);
+        }
 
-		// performs move only if miner is not facing edge 
-		if(!manager.isMinerFacingEdge(quarry, miner)) {
-			moveMiner.move();
-			moveMiner.setHeuristicValue(moveMiner.scan(quarry));
-		}
+        this.smartFrame.add(scrollPane);
+        this.smartFrame.setTitle("Smart Rational Level Miner");
+        this.smartFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.smartFrame.setLocationRelativeTo(null); // makes window open on center
+        this.smartFrame.setVisible(true);
+    }
 
-		rotateMiner.rotate();
-		rotateMiner.setHeuristicValue(rotateMiner.scan(quarry));
+    public class ButtonListener implements ActionListener {
 
-		// pushes only rotateMiner if miner is facing edge (OOB) or node has already been explored
-		if(manager.isMinerFacingEdge(quarry, miner) ||
-				isInList(exploredNodes, moveMiner) && !isInList(exploredNodes, rotateMiner)) {
-			nodeList.push(rotateMiner);
-		}
-		// pushes only moveMiner if node has already been explored
-		else if(isInList(exploredNodes, rotateMiner) && !isInList(exploredNodes, rotateMiner)) {
-			nodeList.push(moveMiner);
-		}
-		// check if moveNode's h(n) >= rotateNode's h(n)
-		// whichever is pushed last becomes top of stack
-		else if(moveMiner.getHeuristicValue() >= rotateMiner.getHeuristicValue()) {
-			nodeList.push(rotateMiner); // push rotate node
-			nodeList.push(moveMiner); // push move node
-		}
-		else {
-			nodeList.push(moveMiner); // push move node
-			nodeList.push(rotateMiner); // push rotate node
-		}
-	}
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            sliderFrame.setVisible(false);
+            createSmartFrame();
+        }
+    }
 
-	// copies position and orientation of m2 to m1
-	private void copy(Miner m1, Miner m2) {
-		m1.setX(m2.getX());
-		m1.setY(m2.getY());
-		m1.setFront(m2.getFront());
-	}
+    public class SliderListener implements ChangeListener {
 
-	@Override
-	public void paintComponent(Graphics g) {
-
-		for(int i = 0; i < quarry.getSize(); i++) {
-			for(int j = 0; j < quarry.getSize(); j++) {
-
-				int x = j * IMG_SIZE;
-				int y = i * IMG_SIZE + OFFSET;
-				Piece currentPiece = this.quarry.getPiece(i, j);
-
-				// draw all other pieces
-				if(currentPiece instanceof Stone)
-					g.drawImage(this.images[STONE], x, y, this);
-				else if(currentPiece instanceof Pit)
-					g.drawImage(this.images[PIT], x, y, this);
-				else if(currentPiece instanceof Beacon) {
-					g.drawImage(this.images[BEACON], x, y, this);
-					int num = ((Beacon) currentPiece).getSquaresToGold();
-					g.drawString("" + num, x + 10, y + 25);
-				}
-				else if(currentPiece instanceof PotOfGold)
-					g.drawImage(this.images[GOLD], x, y, this);
-			}
-		}
-
-		// draw miner
-		g.drawImage(this.images[miner.getFront()],
-				miner.getY() * IMG_SIZE,
-				miner.getX() * IMG_SIZE + OFFSET,
-				this);
-	}
-
-	private class FastPlay implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			bestFirstSearch();
-		}
-	}
-
-	private class OneStep implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if(!nodeList.isEmpty() && !manager.isOnPotOfGold(quarry, miner)) {
-				miner = nodeList.peek();
-				exploredNodes.add(nodeList.pop());
-				expand();
-			}
-		}
-	}
-
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            size = slider.getValue();
+            sizeLabel.setText("Selected Size: " + size);
+        }
+    }
 }
