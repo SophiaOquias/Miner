d017893b98322d0972db0291126957694e29f744
Sophia <sophia.oquias@gmail.com>
2021-11-22 15:56:45 +0800

    Added GUI components

--- src/com/mine/userinterface/SmartGUI.java
+++ src/com/mine/userinterface/SmartGUI.java
@@ -6,7 +6,10 @@ import com.mine.miner.Miner;
 import com.mine.miner.MiningManager;
 
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 
 public class SmartGUI extends JPanel {
@@ -15,13 +18,19 @@ public class SmartGUI extends JPanel {
 	private Quarry quarry;
 	private Miner miner;
 	private MiningManager manager;
-	private Image[] images;
 	private Stack<Miner> nodeList;
 	private ArrayList<Miner> exploredNodes;
+
+	//GUI Components
+	private Image[] images;
+	private Timer timer;
+	private JButton playButton;
+	private JButton stepButton;
 	
 	//Constants
 	private final int IMG_SIZE = 30;
 	private final int OFFSET = 50;
+	private final int DELAY = 140;
 	
 	//Image constants
 	private final int NUM_IMG = 8;
@@ -32,15 +41,46 @@ public class SmartGUI extends JPanel {
 	private final int GOLD = 7;
 	
 	public SmartGUI(int size) {
+		this.initBoard(size);
+		this.loadImages();
+
+		this.setPreferredSize(
+				new Dimension(
+						quarry.getSize() * IMG_SIZE,
+						quarry.getSize() * IMG_SIZE + OFFSET
+				)
+		);
+
+		this.timer = new Timer(DELAY, new FastPlay());
+
+		this.playButton = new JButton("Play");
+		this.stepButton = new JButton("Play One Step");
+
+		this.playButton.addActionListener(new FastPlay());
+
+		this.add(this.playButton);
+	}
+
+	private void initBoard(int size) {
 		this.quarry = new Quarry(size);
 		this.miner = new Miner();
 		this.manager = new MiningManager(quarry, miner);
+
 		this.nodeList = new Stack<>();
 		this.nodeList.push(this.miner);
 		this.exploredNodes = new ArrayList<>();
 	}
+
+	private void loadImages() {
+		images = new Image[NUM_IMG];
+
+		for(int i = 0; i < NUM_IMG; i++) {
+			var path = "src/images/" + i + ".png";
+			this.images[i] = (new ImageIcon(path)).getImage().getScaledInstance(IMG_SIZE, IMG_SIZE, 1);
+		}
+	}
 	
-	public boolean minerCompare(Miner m1, Miner m2) {
+	private boolean minerCompare(Miner m1, Miner m2) {
 	    if(m1.getX() == m2.getX() &&
 	        m1.getY() == m2.getY() &&
 	        m1.getFront() == m2.getFront())
@@ -48,7 +88,7 @@ public class SmartGUI extends JPanel {
 	   return false;
 	}
 
-	public boolean isInList(ArrayList<Miner> nodeList, Miner node) {
+	private boolean isInList(ArrayList<Miner> nodeList, Miner node) {
 		for(int i = 0; i < nodeList.size(); i++) {
 		    if(minerCompare(nodeList.get(i), node))
 		        return true; 
@@ -109,6 +149,45 @@ public class SmartGUI extends JPanel {
 		m1.setX(m2.getX());
 		m1.setY(m2.getY());
 		m1.setFront(m2.getFront());
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		for(int i = 0; i < quarry.getSize(); i++) {
+			for(int j = 0; j < quarry.getSize(); j++) {
+
+				int x = j * IMG_SIZE;
+				int y = i * IMG_SIZE + OFFSET;
+				Piece currentPiece = this.quarry.getPiece(i, j);
+
+				// draw all other pieces
+				if(currentPiece instanceof Stone)
+					g.drawImage(this.images[STONE], x, y, this);
+				else if(currentPiece instanceof Pit)
+					g.drawImage(this.images[PIT], x, y, this);
+				else if(currentPiece instanceof Beacon) {
+					g.drawImage(this.images[BEACON], x, y, this);
+					int num = ((Beacon) currentPiece).getSquaresToGold();
+					g.drawString("" + num, x + 10, y + 25);
+				}
+				else if(currentPiece instanceof PotOfGold)
+					g.drawImage(this.images[GOLD], x, y, this);
+			}
+		}
+
+		// draw miner
+		g.drawImage(this.images[miner.getFront()],
+				miner.getY() * IMG_SIZE,
+				OFFSET + miner.getX() * IMG_SIZE,
+				this);
+	}
+
+	private class FastPlay implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			bestFirstSearch();
+		}
 	}
 
 }
